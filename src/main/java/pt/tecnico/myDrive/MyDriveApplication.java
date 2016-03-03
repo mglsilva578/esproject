e4b9341f41147c61ccf6ff560fefc6a01ae96ba0
R3Moura <ricardo.moura93@gmail.com>
2016-03-03 09:15:35 +0000

    first generation dml

---
+++ src/main/java/pt/tecnico/myDrive/MyDriveApplication.java
@@ -0,0 +1,39 @@
+package pt.tecnico.myDryve;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.File;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.DomainRoot;
+import pt.ist.fenixframework.FenixFramework;
+
+public class MyDriveApplication {
+    // FenixFramework will try automatic initialization when first accessed
+    public static void main(String [] args) {
+        try {
+            applicationCodeGoesHere();
+        } finally {
+            // ensure an orderly shutdown
+            FenixFramework.shutdown();
+        }
+    }
+
+    public static void applicationCodeGoesHere() {
+        someTransaction();
+    }
+
+    @Atomic
+    public static void someTransaction() {
+        System.out.println("FenixFramework's root object is: " + FenixFramework.getDomainRoot());
+    }
+}
