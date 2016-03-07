eafcc5ed5457068b04c63bc58bf4e5738a41c897
Diogo Fernandes <diogofafernandes@ist.utl.pt>
2016-03-07 12:49:16 +0000

    first attempt of issue #1

--- src/main/java/pt/tecnico/myDrive/domain/Dir.java
+++ src/main/java/pt/tecnico/myDrive/domain/Dir.java
@@ -1,9 +1,28 @@
 package pt.tecnico.myDrive.domain;
 
+import java.util.Set;
+
 public class Dir extends Dir_Base {
-    
-    public Dir() {
-        super();
-    }
-    
+
+	public Dir() {
+		super();
+	}
+
+	public String listDirContent(String path){
+		String array[];
+		array = path.split("/",2);
+		String arg1 = array[0];
+		String arg2 = array[1];
+		if (arg2==null){
+			for(File file: getFileSet())
+				System.out.println(file.getName());
+		}
+		for(File file: getFileSet()){
+			if(file.getName().equals(arg1))
+				((Dir) file).listDirContent(arg2);
+		}
+		return null;
+
+
+	}
 }
