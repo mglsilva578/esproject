f7a50e332d4d1d754d826d2e6b89284507112363
Jose Luis Sacadura Valente de Valle-Flor <jose.luis.vf.tagus@gmail.com>
2016-03-09 09:50:47 +0000

    This closes #7.

--- src/main/java/pt/tecnico/myDrive/domain/User.java
+++ src/main/java/pt/tecnico/myDrive/domain/User.java
@@ -5,10 +5,24 @@ public class User extends User_Base {
 	protected User(){super();}
 	
     public User(String username, String password, String name, String mask) {
-        setUsername(username);
+    	if(isValidUsername(username)){
+    		setUsername(username);    		
+    	}else{
+    		//TODO lancar aqui a excepcao adequada
+    	}
         setPassword(password);
         setName(name);
         setMask(mask);
     }
+
+	private boolean isValidUsername(String username) {
+		if(username == null) return false;
+		return !(username.isEmpty() || isComposedOnlyLettersDigits(username));
+	}
+
+	private boolean isComposedOnlyLettersDigits(String username) {
+		String patternNotToMatch = "[a-zA-Z0-9]*[^(a-zA-Z0-9)]+[a-zA-Z0-9]*";
+		return !username.matches(patternNotToMatch);
+	}
     
 }
