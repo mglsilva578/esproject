5aad7cc158a8899f9e0e0f03d345af8219227ef0
R3Moura <ricardo.moura93@gmail.com>
2016-03-10 19:45:13 +0000

    created getFileByName and hasFile

--- src/main/java/pt/tecnico/myDrive/domain/MyDrive.java
+++ src/main/java/pt/tecnico/myDrive/domain/MyDrive.java
@@ -3,18 +3,33 @@ package pt.tecnico.myDrive.domain;
 import pt.ist.fenixframework.FenixFramework;
 
 public class MyDrive extends MyDrive_Base {
-    
-    private MyDrive() {
-        super();
-        setRoot(FenixFramework.getDomainRoot());
-    }
-    
-    public static MyDrive getInstance(){
-    	MyDrive drive = FenixFramework.getDomainRoot().getMydrive();
-    	if( drive != null ){
-    		return drive;
-    	}else{
-    		return new MyDrive();
-    	}
-    }
+
+	private MyDrive() {
+		super();
+		setRoot(FenixFramework.getDomainRoot());
+	}
+
+	public static MyDrive getInstance(){
+		MyDrive drive = FenixFramework.getDomainRoot().getMydrive();
+		if( drive != null ){
+			return drive;
+		}else{
+			return new MyDrive();
+		}
+	}
+	
+	public File getFileByName(String name) {
+		for (File file : getFileSet()) {
+			if (file.getName().equals(name)) {
+				return file;
+			}
+		}
+		return null;
+	}
+
+	public boolean hasFile(String Name) {
+		return getFileByName(Name) != null;
+	}
+		
+
 }
