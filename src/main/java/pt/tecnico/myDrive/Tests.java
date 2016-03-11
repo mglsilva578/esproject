e56fed15823e280e20aa9e9a6dcb11ccb0066162
Miguel Costa e Silva <mglsilva578@gmail.com>
2016-03-11 15:35:13 +0000

    Test Class #8

---
+++ src/main/java/pt/tecnico/myDrive/Tests.java
@@ -0,0 +1,44 @@
+package pt.tecnico.myDrive;
+
+import pt.tecnico.myDrive.domain.Dir;
+import pt.tecnico.myDrive.domain.File;
+import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.PlainFile;
+import pt.tecnico.myDrive.domain.User;
+
+public class Tests {
+	
+	//Criar o ficheiro de texto /home/README com o conteúdo lista de utilizadores
+	public static void Teste01(String username,MyDrive md){
+		String content = "utilizador1, utilizador2";
+		String path = "/Home/README";
+		String permissions = "rw";
+		String name = "nome";
+		User owner = md.getInstance().getUserByUsername(username);
+		PlainFile pf = new PlainFile(md, owner, name, permissions, content);
+		
+	}
+	//criar a directoria /usr/local/bin
+	public static void Teste02(String username, MyDrive md){
+		User owner = md.getInstance().getUserByUsername(username);
+		String name = "nome";
+		String permissions = "rw";
+		Dir dir = new Dir(md, owner, name, permissions);
+	}
+	//imprimir o conteudo do ficheiro /home/README
+	public static void Teste03(String filename,  MyDrive md){
+		for(File file : md.getInstance().getFileSet()){
+			if(filename.equals(file.getName()))
+				System.out.println(file.toString());
+		}
+	}
+	
+	//Remover a diretoria /usr/local/bin
+	public static void Teste04(){}
+	//Imprimir a exportacao em XML do sistema de ficheiros
+	public static void Teste05(){}
+	//Remover o ficheiro /home/README
+	public static void Teste06(){}
+	//Imprimir a listagem simples da directoria /home
+	public static void Teste07(){}
+}
