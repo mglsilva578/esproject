98484fd2acab8891bdaa4943f446708c4a944c74
Miguel Costa e Silva <mglsilva578@gmail.com>
2016-03-11 11:30:39 +0000

    Commit to sync with master #4

---
+++ src/main/java/pt/tecnico/myDrive/exception/ImportDocumentException.java
@@ -0,0 +1,10 @@
+package pt.tecnico.myDrive.exception;
+
+public class ImportDocumentException extends MyDriveException {
+
+	private static final long serialVersionUID = 7454896768786230954L;
+	
+	public ImportDocumentException(String message){
+		super("ImportXml is invalid - " + message);
+	}
+}
