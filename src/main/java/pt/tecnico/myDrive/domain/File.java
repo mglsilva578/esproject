e935dea36c6cadad891a3bbffa7287871a5284b3
Diogo Fernandes <diogofafernandes@ist.utl.pt>
2016-03-13 16:47:46 +0000

    Fix getPath

--- src/main/java/pt/tecnico/myDrive/domain/File.java
+++ src/main/java/pt/tecnico/myDrive/domain/File.java
@@ -16,7 +16,7 @@ public class File extends File_Base {
 	public File(MyDrive drive, User owner, String name, String permissions){
 		this.init(drive, owner, name, permissions);
 	}
-	
+
 	public File(MyDrive drive, User owner, String name, String permissions, Dir dir){
 		this.init(drive, owner, name, permissions, dir);
 	}
@@ -29,7 +29,7 @@ public class File extends File_Base {
 		this.setMydrive(drive);
 		this.setOwner(owner);
 	}
-	
+
 	protected void init(MyDrive drive, User owner, String name, String permissions, Dir dir){
 		setId(drive.getNewFileId());
 		setName(name);
@@ -40,70 +40,65 @@ public class File extends File_Base {
 		dir.addFile(this);
 	}
 
-	public String getPath(MyDrive drive, String p){
-		String path = null;
-		if(!(this.getFather().equals(drive.getRootDir()))){
-			path = this.getName() + p; 
-			this.getFather().getPath(drive, path);
-			return null;
-		}
-		else{
-			return path;
-		}
-	}
-	
-	@Override
-	public void setName(String n){
-		if(!(this.getId() == SLASH_DIR_1 )){
-			if(n.contains("/") || n.contains("\0")){
-				throw new InvalidFileNameException(n);
-			}
-		}
-		super.setName(n);
+	public String getPath(){
+		if (!this.getName().equals("home"))
+			return getFather().getPath()+"/"+this.getName();
+		return "/"+this.getName();
 	}
 
-	@Override
-	public void setMydrive(MyDrive drive){
-		if (drive == null){
-			super.setMydrive(null);
-		}
-		else{
-			drive.addFile(this);
-		}
-	}
 
-	@Override
-	public void setOwner(User owner){
-		if (owner == null){
-			super.setOwner(null);
-		}
-		else{
-			owner.addFile(this);
+@Override
+public void setName(String n){
+	if(!(this.getId() == SLASH_DIR_1 )){
+		if(n.contains("/") || n.contains("\0")){
+			throw new InvalidFileNameException(n);
 		}
 	}
-	
-	public void remove(){
-		setMydrive(null);
-		setOwner(null);
-		deleteDomainObject();
-	}
+	super.setName(n);
+}
 
-	@Override
-	public String toString(){
-		String description = "\n";
-		description += "\tid: " + this.getId() + "\n";
-		description += "\tname: " + this.getName() + "\n";
-		description += "\towner: " + this.getOwner().getUsername() + "\n";
-		description += "\tlast modified: " + this.getLast_modification() + "\n";
-		description += "\tpermissions: " + this.getPermissions() + "\n";
-		return description;
+@Override
+public void setMydrive(MyDrive drive){
+	if (drive == null){
+		super.setMydrive(null);
 	}
+	else{
+		drive.addFile(this);
+	}
+}
 
-	public void importXML(Element elm){
-		this.setName(elm.getAttributeValue("name"));
-		User user = FenixFramework.getDomainRoot().getMydrive().getUserByUsername(elm.getAttributeValue("owner"));
-		this.setOwner(user);
-		this.setPermissions(elm.getAttributeValue("permissions"));
+@Override
+public void setOwner(User owner){
+	if (owner == null){
+		super.setOwner(null);
 	}
+	else{
+		owner.addFile(this);
+	}
+}
+
+public void remove(){
+	setMydrive(null);
+	setOwner(null);
+	deleteDomainObject();
+}
+
+@Override
+public String toString(){
+	String description = "\n";
+	description += "\tid: " + this.getId() + "\n";
+	description += "\tname: " + this.getName() + "\n";
+	description += "\towner: " + this.getOwner().getUsername() + "\n";
+	description += "\tlast modified: " + this.getLast_modification() + "\n";
+	description += "\tpermissions: " + this.getPermissions() + "\n";
+	return description;
+}
+
+public void importXML(Element elm){
+	this.setName(elm.getAttributeValue("name"));
+	User user = FenixFramework.getDomainRoot().getMydrive().getUserByUsername(elm.getAttributeValue("owner"));
+	this.setOwner(user);
+	this.setPermissions(elm.getAttributeValue("permissions"));
+}
 
 }
