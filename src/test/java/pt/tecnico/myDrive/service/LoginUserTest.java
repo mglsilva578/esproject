f0bc46ea74e8bb8e88cfa0784ad0161c002cfa0d
Jose Luis Sacadura Valente de Valle-Flor <jose.luis.vf.tagus@gmail.com>
2016-03-31 13:57:43 +0100

    Coded LoginUserTest. Closes #37.
    Test is supposed to fail until the service (and necessary domain
    changes) are fully implemented.

---
+++ src/test/java/pt/tecnico/myDrive/service/LoginUserTest.java
@@ -0,0 +1,186 @@
+package pt.tecnico.myDrive.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
+import org.junit.Test;
+
+import pt.tecnico.myDrive.domain.LoginManager;
+import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.Session;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exception.CannotListTokenException;
+import pt.tecnico.myDrive.exception.InvalidPasswordException;
+import pt.tecnico.myDrive.exception.InvalidTokenException;
+import pt.tecnico.myDrive.exception.InvalidUsernameException;
+
+public class LoginUserTest extends AbstractServiceTest{
+
+	@Override
+	protected void populate() {
+		MyDrive myDrive = MyDrive.getInstance();
+		new User(myDrive, "zttr", "76534", "Diogo", "rwxd----", null);
+		new User(myDrive, "mglsilva578", "68230", "Miguel", "rwxd----", null);
+		new User(myDrive, "R3Moura", "74005", "Ricardo", "rwxd----", null);
+		new User(myDrive, "joseluisvf", "55816", "JoseLuis", "rwxd----", null);
+		new User(myDrive, "ist176544", "76544", "Daniel", "rwxd----", null);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void createSessionsForDifferentUsers() {
+		String username1, username2, username3, username4, username5,
+		password1, password2, password3, password4, password5;
+		LoginUserService service;
+		Long token1, token2, token3, token4, token5;
+		MyDrive myDrive = MyDrive.getInstance();
+		LoginManager loginManager = myDrive.getLoginManager();
+		
+		int howManySessionsBefore = loginManager.getSessionsCount();
+		
+		username1 = "zttr";
+		password1 = "76534";
+		service = new LoginUserService(username1, password1);
+		service.execute();
+		token1 = service.getResult();
+		
+		username2 = "mglsilva578";
+		password2 = "68230";
+		service = new LoginUserService(username2, password2);
+		service.execute();
+		token2 = service.getResult();
+		
+		username3 = "R3Moura";
+		password3 = "74005";
+		service = new LoginUserService(username3, password3);
+		service.execute();
+		token3 = service.getResult();
+		
+		username4 = "joseluisvf";
+		password4 = "55816";
+		service = new LoginUserService(username4, password4);
+		service.execute();
+		token4 = service.getResult();
+		
+		username5 = "ist176544";
+		password5 = "76544";
+		service = new LoginUserService(username5, password5);
+		service.execute();
+		token5 = service.getResult();
+		
+		// Confirm everything went well
+		int howManySessionsAfter = loginManager.getSessionsCount();
+		assertEquals("Number of sessions before and after creation is not consistent.", howManySessionsBefore, howManySessionsAfter - 5);
+		
+		Session session = loginManager.getSessionByToken(token1);
+		assertEquals("Username of session's owner does not match that of its creator.",username1, session.getOwner().getUsername());
+		session = loginManager.getSessionByToken(token2);
+		assertEquals("Username of session's owner does not match that of its creator.",username2, session.getOwner().getUsername());
+		session = loginManager.getSessionByToken(token3);
+		assertEquals("Username of session's owner does not match that of its creator.",username3, session.getOwner().getUsername());
+		session = loginManager.getSessionByToken(token4);
+		assertEquals("Username of session's owner does not match that of its creator.",username4, session.getOwner().getUsername());
+		session = loginManager.getSessionByToken(token5);
+		assertEquals("Username of session's owner does not match that of its creator.",username5, session.getOwner().getUsername());
+	}
+	
+	@Test
+	public void confirmLastActiveAtChangedAfterUsingSession(){
+		String username = "joseluisvf";
+		String password = "55816";
+		Long token = null;
+		long waitDurationInMillis = 5000;
+		MyDrive myDrive = MyDrive.getInstance();
+		LoginManager loginManager = myDrive.getLoginManager();
+		
+		LoginUserService service = new LoginUserService(username, password);
+		token = service.getResult();
+		Session session = loginManager.getSessionByToken(token);
+
+		DateTime lastActiveAtCreation = session.getLastActiveAt();
+		try {
+			Thread.sleep(waitDurationInMillis);
+			loginManager.getSessionByToken(token);
+			DateTime lastActiveAfterCreation = session.getLastActiveAt();
+			int howManySecondsHavePassed = Seconds.secondsBetween(lastActiveAtCreation, lastActiveAfterCreation).getSeconds();
+			assertEquals("Inconsistent number of seconds have passed.", howManySecondsHavePassed, waitDurationInMillis);
+		} catch(InterruptedException ex) {
+			Thread.currentThread().interrupt();
+			log.warn("Thread was interrupted for which reason the failure of this test cannot be accounted for.");
+		}
+	}
+	
+	@Test
+	public void createTwoSessionsForSameUser(){
+		String username = "joseluisvf";
+		String password = "55816";
+		Long token = null;
+		MyDrive myDrive = MyDrive.getInstance();
+		LoginManager loginManager = myDrive.getLoginManager();
+		
+		LoginUserService service = new LoginUserService(username, password);
+		token = service.getResult();
+		Session firstSession = loginManager.getSessionByToken(token);
+		
+		service = new LoginUserService(username, password);
+		token = service.getResult();
+		Session secondSession = loginManager.getSessionByToken(token);
+		
+		assertEquals("Owners should be the same but aren't.",firstSession.getOwner(), secondSession.getOwner());
+	}
+	
+	@Test(expected = InvalidTokenException.class)
+	public void useInvalidToken(){
+		String username = "joseluisvf";
+		String password = "55816";
+		Long token = null;
+		Long unexistentToken = new Long(1);
+		MyDrive myDrive = MyDrive.getInstance();
+		LoginManager loginManager = myDrive.getLoginManager();
+		
+		LoginUserService service = new LoginUserService(username, password);
+		token = service.getResult();
+
+		while(unexistentToken == token){
+			unexistentToken = new Long(new BigInteger(64, new Random()).longValue());
+		}
+		loginManager.getSessionByToken(unexistentToken);
+	}
+	
+	@Test(expected = InvalidPasswordException.class)
+	public void createSessionWithInvalidPassword(){
+		String username = "joseluisvf";
+		String password = "00000";
+		
+		LoginUserService service = new LoginUserService(username, password);
+		service.execute();
+	}
+	
+	@Test(expected = InvalidUsernameException.class)
+	public void createSessionWithInvalidUsername(){
+		String username = "vfjoseluis";
+		String password = "55816";
+		
+		LoginUserService service = new LoginUserService(username, password);
+		service.execute();
+	}
+	
+	@Test(expected = CannotListTokenException.class)
+	public void tryToListTokens(){
+		String username = "joseluisvf";
+		String password = "55816";
+		MyDrive myDrive = MyDrive.getInstance();
+		LoginManager loginManager = myDrive.getLoginManager();
+		
+		LoginUserService service = new LoginUserService(username, password);
+		service.execute();
+		Long token = service.getResult();
+		Session session = loginManager.getSessionByToken(token);
+		User owner = session.getOwner();
+		owner.getSessionsSet();
+	}
+}
