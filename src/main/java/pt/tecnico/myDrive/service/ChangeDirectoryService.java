a78f6e5e9b3e9af8a1e00bec37c02a50d2c83dc3
diogo fernandes <diogofafernandes@ist.utl.pt>
2016-04-11 10:16:39 +0100

    Closes #25

--- src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
@@ -15,6 +15,7 @@ public class ChangeDirectoryService extends MyDriveService {
 	public ChangeDirectoryService(Long token,String path){
 		this.token=token;
 		this.path=path;
+		
 	}
 	@Override
 	protected void dispatch() throws MyDriveException {
