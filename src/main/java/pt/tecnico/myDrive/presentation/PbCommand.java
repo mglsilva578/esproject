b81775df35979d8807af81eba0ee0f44c112b0a4
R3Moura <ricardo.moura93@gmail.com>
2016-04-25 12:20:20 +0100

    create skeleton of presentation layer

---
+++ src/main/java/pt/tecnico/myDrive/presentation/PbCommand.java
@@ -0,0 +1,11 @@
+package pt.tecnico.myDrive.presentation;
+
+public abstract class PbCommand extends Command {
+	public PbCommand(Shell sh, String n){ 
+		super(sh, n); 
+	}
+
+	public PbCommand(Shell sh, String n, String h){ 
+		super(sh, n, h);
+	}
+}
---
+++ src/main/java/pt/tecnico/myDrive/presentation/PbShell.java
@@ -0,0 +1,14 @@
+package pt.tecnico.myDrive.presentation;
+
+public class PbShell extends Shell{
+
+	public static void main(String[] args) throws Exception{
+		PbShell sh = new PbShell();
+		sh.execute();
+	}
+
+	public PbShell(){
+		super("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMyDrive");
+
+	}
+}
