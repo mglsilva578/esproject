e5e8cba785805deff2b0ae15808183d35cd529a8
Jose Luis Sacadura Valente de Valle-Flor <jose.luis.vf.tagus@gmail.com>
2016-04-28 11:59:59 +0100

    Protected Session from giving access to Tokens.
    Closes #88 (had done this before issue was created), Closes #96.

--- src/main/java/pt/tecnico/myDrive/exception/CannotListTokenException.java
+++ src/main/java/pt/tecnico/myDrive/exception/CannotListTokenException.java
@@ -5,6 +5,6 @@ public class CannotListTokenException extends MyDriveException {
 	private static final long serialVersionUID = 1L;
 	
 	public CannotListTokenException(){
-		super("Listing of tokens ( or sessions ) is not permitted");
+		super("Listing (or retrieval) of tokens (or sessions) is not permitted");
 	}
 }
