f9d145bfa8ac396b6988f82f8e1d435736a69ca7
diogo fernandes <diogofafernandes@ist.utl.pt>
2016-04-29 14:42:06 +0100

    Closes #65

--- src/main/java/pt/tecnico/myDrive/service/AddEnvironmentVariableService.java
+++ src/main/java/pt/tecnico/myDrive/service/AddEnvironmentVariableService.java
@@ -3,15 +3,25 @@ package pt.tecnico.myDrive.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.myDrive.domain.EnvironmentVariable;
+import pt.tecnico.myDrive.domain.Session;
+import pt.tecnico.myDrive.exception.InvalidEnvironmentVariableNameException;
+import pt.tecnico.myDrive.exception.InvalidEnvironmentVariableValueException;
+import pt.tecnico.myDrive.exception.InvalidTokenException;
 import pt.tecnico.myDrive.exception.MyDriveException;
 import pt.tecnico.myDrive.service.dto.EnvironmentVariableDto;
+
+/* Add an environment variable or redefine an existing one with the same name. 
+ * This service requires a token, the variable name and its value. 
+ * The service returns a list of all defined environment variables.*/
 
 public class AddEnvironmentVariableService extends MyDriveService{
 	private Long token;
 	private String environmentVariableName;
 	private String environmentVariableValue;
 	private List<EnvironmentVariableDto> existingEnvironmentVariables;
-	
+	private Session session;
 	public AddEnvironmentVariableService(
 			Long token,
 			String environmentVariableName,
@@ -19,15 +29,49 @@ public class AddEnvironmentVariableService extends MyDriveService{
 		this.token = token;
 		this.environmentVariableName = environmentVariableName;
 		this.environmentVariableValue = environmentVariableValue;
+		//this.session=getSession(token);
+		this.existingEnvironmentVariables = new ArrayList<EnvironmentVariableDto>();
+
 	}
-	
+
 	@Override
 	protected void dispatch() throws MyDriveException {
 		//TODO implement service logic.
-		this.existingEnvironmentVariables = new ArrayList<EnvironmentVariableDto>();
+		session=getSession(token);
+		if(checkArgs(token, environmentVariableName, environmentVariableValue)){
+			EnvironmentVariable newenv = new EnvironmentVariable(session,environmentVariableName,environmentVariableValue);
+			session.addEnvironmentVariables(newenv);
+		}
+
+		for(EnvironmentVariable env : session.getEnvironmentVariablesSet()){
+			EnvironmentVariableDto newenvdto = new EnvironmentVariableDto(env.getName(), env.getValue());
+			existingEnvironmentVariables.add(newenvdto);
+		}
 	}
-	
+
+
+
 	public List<EnvironmentVariableDto> getResult() {
-		return this.existingEnvironmentVariables;
+		return existingEnvironmentVariables;
+	}
+
+
+	public Session getSession(Long token){
+		Session session = getMyDrive().getLoginManager().getSessionByToken(this.token);
+		return session;
+	}
+
+	public boolean checkArgs(Long token, String environmentVariableName,String environmentVariableValue){
+		if(token==null){
+			throw new InvalidTokenException(token);
+		}
+		if(environmentVariableName==null){
+			throw new InvalidEnvironmentVariableNameException(environmentVariableName);
+		}
+		if(environmentVariableValue==null){
+			throw new InvalidEnvironmentVariableValueException(environmentVariableValue);
+		}
+		else
+			return true;
 	}
 }
